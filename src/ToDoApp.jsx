8bfed246ca0361bcefaac07bba894a7fce187cf1
Elena Pavlova <lenlho22@gmail.com>
2017-12-18 13:18:17 +0300

    Init project code

---
+++ src/ToDoApp.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+export default class TodoApp extends React.Component {
+    render()  {
+        return (
+            <div>
+                Hello 222!!
+            </div>
+        );
+    }
+};
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import ToDoApp from './ToDoApp.jsx'
+
+ReactDOM.render(
+    <ToDoApp />,
+    document.getElementById('mount-point')
+);
